fbb917d21f9cb715ebae2e263ca62517eeb36ea3
Marek Martinak <123459150+Aang1214@users.noreply.github.com>
2023-11-29 12:27:47 +0000

    Add project files.

---
+++ CA2_Algorithm/CA2_Algorithm.cpp
@@ -0,0 +1,26 @@
+// CA2_Algorithm.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    string s = "<dir>< name > ADS_Single_LinkedList_Exercises< / name><dir><name>.git< / name><file><name>config< / name>< length>353 b< / length><type>config< / type>< / file><file><name>description< / name>< length>73 b< / length><type>description< / type>< / file><file><name>HEAD< / name>< length>23 b< / length><type>HEAD< / type>< / file>< / dir><dir><name>.vs< / name><dir><name>ADS_Single_LinkedList_Exercises< / name><dir><name>v17< / name>< / dir>< / dir>< / dir><dir><name>Debug< / name><file><name>SinglyLinkedList.pdb< / name>< length>978944 b< / length><type>pdb< / type>< / file><file><name>TestSinglyLinkedList.exp< / name>< length>41528 b< / length><type>exp< / type>< / file><file><name>TestSinglyLinkedList.pdb< / name>< length>1945600 b< / length><type>pdb< / type>< / file>< / dir>< / dir>";
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
